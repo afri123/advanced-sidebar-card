5998d6d8cf2044ba94de716e53516187443ef91d
afri123 <80269996+afri123@users.noreply.github.com>
2026-04-07 21:59:13 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,16 @@
-import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import serve from 'rollup-plugin-serve';
-import json from '@rollup/plugin-json';
-
-const dev = process.env.ROLLUP_WATCH;
-
-const plugins = [
-  nodeResolve(),
-  commonjs(),
-  typescript({
-    typescript: require('typescript'),
-    objectHashIgnoreUnknownHack: true,
-    tsconfigOverride: { compilerOptions: { sourceMap: false } },
-  }),
-  json(),
-  babel({
-    exclude: 'node_modules/**',
-  }),
-  dev && serve({
-    contentBase: ['./dist'],
-    host: '0.0.0.0',
-    port: 5005,
-    allowCrossOrigin: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  }),
-  !dev && terser(),
-].filter(Boolean);
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/sidebar-card.ts',
+  input: 'src/advanced-sidebar-card.ts',
   output: {
-    dir: 'dist',
-    format: 'es',
-    sourcemap: false,
+    file: 'advanced-sidebar-card.js',
+    format: 'es'
   },
-  plugins,
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript()
+  ]
 };
